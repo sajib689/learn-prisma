9d0b02b00578f83b84f03928a62fa5df66712dff
sajib689 <sajibbabu751@gmail.com>
2026-05-24 20:35:54 +0600

    refactor: replace user upsert and delete logic with user creation

--- index.ts
+++ index.ts
@@ -1,32 +1,13 @@
 import { prisma } from "./lib/prisma";
 
 async function main() {
-  // update or create a user
-  const user = await prisma.user.upsert({
-    where: {
-      email: "john.doe@example.com",
-    },
-    update: {
-      name: "Jane Doe",
-      profilePhoto: "https://example.com/profile-photo.jpg",
-    },
-    create: {
-      name: "John Doe",
-      email: "john.doe@example.com",
-      profilePhoto: "https://example.com/profile-photo.jpg",
+  const result = await prisma.user.create({
+    data: {
+      name: "Sajib",
+      email: "sajibbabu751@gmail.com",
     },
   });
-  console.log("Upserted user:", user);
-  // get all users
-  const allUsers = await prisma.user.findMany();
-  //  delete a user
-  const deleteUser = await prisma.user.delete({
-    where: {
-      id: 1,
-    },
-  });
-  console.log("Deleted user:", deleteUser);
-  console.log("All users:", JSON.stringify(allUsers, null, 2));
+  console.log(result);
 }
 
 main()
